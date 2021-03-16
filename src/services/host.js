7508f6cb8f5eeb95126bc92d161d7ab4684fce2a
Alessandro L. Santos <aleluizsantos@gmail.com>
2021-03-16 13:27:39 -0300

    changed host

--- src/services/host.js
+++ src/services/host.js
@@ -1,2 +1,2 @@
-export const url = "http://192.168.0.8:3333";
-// export const url = "https://premiumcasacarne.herokuapp.com";
+// export const url = "http://192.168.0.8:3333";
+export const url = "https://premiumcasacarne.herokuapp.com";
